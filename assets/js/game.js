7f35e1eb0ae9462e05202a923b3d5cf738e9a5ef
Shmee <smaheilman@gmail.com>
2021-09-02 21:36:14 -0400

    window alert math

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is you robot's name?");
+
+// Note the lack of quotation marks around playerName 
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+//this will do math and log 20
+console.log(10+10);
+
+// this creates a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
